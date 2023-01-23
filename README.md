06ed85d35339ab76ef5da6cac5a9b524517b9d21
kevinavilacoron <122801450+kevinavilacoronel100@users.noreply.github.com>
2023-01-23 14:53:12 +0000

    se creo el programa1

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 |No.|Archivo|Descripcion|
 |--|--|--|
 |1|programa0.py|Hola mundo en Python3|
+|2|programa1.py|Comentarios multilinea,una linea,concatenacion de dos variables.|
+
---
+++ semana2/programa1.py
@@ -0,0 +1,9 @@
+"""
+    Programa1
+    Nombre: Kevin AC
+    Fecha: 23/01/2023
+    Descripcion:Comentarios multilinea,una linea y concatenacion de dos variables.
+"""
+variable1= "Hola" # variable para almacenar una cadena de caracteres
+variable2= "mundo python" # variable para almacenar una cadena de caracteres
+print(variable1+variable2)
