104a3a6d4c2d2901bce12c0b10046a6903a1bd70
kevinavilacoron <122801450+kevinavilacoronel100@users.noreply.github.com>
2023-01-24 15:00:31 +0000

    |Formatear variables y operaciones

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 |1|programa0.py|Hola mundo en Python3|
 |2|programa1.py|Comentarios multilinea,una linea,concatenacion de dos variables.|
 |3|programa2.py|formatear y identificar errores de llave,syntaxis error.|
-
+|4|programa3.py|Formatear variables y operaciones.|
---
+++ semana2/programa3.py
@@ -0,0 +1,14 @@
+"""
+    Programa2
+    Nombre: Kevin AC
+    Fecha:24/01/23
+    Descripcion:Formatear variables y operaciones.
+    """
+variable1=10 # variable de tipo int
+variable2=5 # variable de tipo int
+print(variable1+variable2) # imprime suma
+print("{}+{}={}".format(variable1,variable2,(variable1+variable2))) # representa la validacion
+print("{}+{}={}".format(variable1,variable2,(variable1+variable2))) # representa la suma de dos variables
+print("El resultado de sumar {}+{}={}".format(variable1,variable2,(variable1+variable2))) # representa la suma de dos variables
+
+
