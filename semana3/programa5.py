2de9cc56255fdb899d8057e90cfa05bd4add93a2
kevinavilacoron <122801450+kevinavilacoronel100@users.noreply.github.com>
2023-01-31 14:07:41 +0000

    Area y Perimetro del triangulo

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -6,12 +6,14 @@
     Area y Perimetro de un triangulo.
 
 """
-print("Area y Perimetro del Triangulo") #un print para que se de un espacion de una linea
+print("Area y Perimetro del Triangulo") #un print para que se de un espacio de una linea
 print("Area del Triangulo")
 b = int(input("ingrese la base: "))
 h = int(input("ingrese altura: "))
 l1 = int(input("ingrese la medida del lado: "))
-A = (b*h)/2 #para sacar el area
-P = l1*3 #para sacar el perimetro
-print("perimetro : ", P)
-print("Area : ", A)
+l2 = int(input("ingrese la medida del lado: "))
+l3 = int(input("ingrese la medida del lado: "))
+a = (b*h)/2 #para sacar el area
+p = l1+l2+l3 #para sacar el perimetro
+print("perimetro : ", a)
+print("Area : ", p)
---
+++ semana3/programa6.py
@@ -0,0 +1,17 @@
+"""
+    Programa6
+    Nombre: Kevin AC
+    Fecha:31/01/2023
+    Descripcion:Area y perimetro del circulo y cuadrado.
+
+"""
+print("Area y perimetro del circulo") #un print para que se de un espacio de una linea
+print("Area del circulo")
+radio=int(input("Radio: "))
+pi=3.1416
+calculo=2*radio*pi
+calculo1=pi*radio**2
+print("El perimero del circulo es: ", calculo)
+print("El area del circulo es: ", calculo1)
+lado=int(input("medida del lado: "))
+calculo2= l*4
